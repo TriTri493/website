d4e2b7cf13957618d5265ba338ced3590fe83801
TriTri493 <72851425+TriTri493@users.noreply.github.com>
2021-02-26 10:31:16 +1100

    Add files via upload

---
+++ pages/showip.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Palpable Coral</title>
+</head>
+<body>
+Your IP Address is: <? echo $_SERVER["REMOTE_ADDR"]; ?><br>
+(i do not keep records of your ip)
+</body>
+</html>
